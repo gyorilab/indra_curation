ba58d8070fadff86dc45f9e731af030fdcb25f09
pagreene <patrick.anton.greene@gmail.com>
2019-12-03 12:54:58 -0500

    Convert file gen to run on webservice.

--- curation_service/app.py
+++ curation_service/app.py
@@ -1,9 +1,8 @@
-import sys
 import pickle
 import argparse
+from os import path
 
-from flask import Flask, request, jsonify, url_for
-from flask_cors import CORS
+from flask import Flask, request, jsonify, url_for, abort
 
 from jinja2 import Environment, ChoiceLoader
 
@@ -12,7 +11,6 @@ from indra.assemblers.html.assembler import loader as indra_loader
 
 
 app = Flask(__name__)
-CORS(app)
 
 
 # Instantiate a jinja2 env.
@@ -23,6 +21,39 @@ env.globals.update(url_for=url_for)
 
 
 CURATIONS = []
+WORKING_DIR = None
+
+
+@app.route('/show/<load_file>', methods=['GET'])
+def load(load_file):
+    load_file = path.join(WORKING_DIR, load_file)
+    print(f"Attempting to load {load_file}")
+    if path.exists(load_file + '.html'):
+        load_file += '.html'
+        print("Using existing html file.")
+        html_filename = load_file
+        with open(html_filename, 'r') as f:
+            content = f.read()
+    elif path.exists(load_file + '.pkl'):
+        load_file += '.pkl'
+        with open(load_file, 'rb') as f:
+            stmts = pickle.load(f)
+        html_assembler = HtmlAssembler(stmts, title='INDRA Curation',
+                                       db_rest_url=request.url_root[:-1])
+        template = env.get_template('curation_service/cur_stmts_view.html')
+        content = html_assembler.make_model(template)
+        html_filename = load_file.replace('.pkl', '.html')
+        with open(html_filename, 'w') as f:
+            f.write(content)
+        print(f"Generated HTML from {len(stmts)} Statements.")
+    else:
+        print(f"Invalid input file: {load_file}")
+        abort(400, (f"Invalid input file: neither {load_file}.pkl or "
+                    f"{load_file}.html exist."))
+        return
+
+    print(f"Presenting {html_filename}")
+    return content
 
 
 @app.route('/curate', methods=['POST'])
@@ -41,39 +72,15 @@ def get_parser():
         description=("Generate and enable curation using an HTML document "
                      "displaying the statements in the given pickle file.")
     )
-    parser.add_argument('load_file',
-                        help=("Either a pickle file containing a list of "
-                              "INDRA Statements, from which an HTML file will "
-                              "be generated, or else a previously-generated "
-                              "HTML file."))
+    parser.add_argument('working_dir',
+                        help=("The directory containing any files you wish "
+                              "to load."))
     return parser
 
 
-def main():
+if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
-
-    if args.load_file.endswith('.pkl'):
-        with open(args.load_file, 'rb') as f:
-            stmts = pickle.load(f)
-        html_assembler = HtmlAssembler(stmts, title='INDRA Curation',
-                                       db_rest_url=request.url_root[:-1])
-        template = env.get_template('curation_service/cur_stmts_view.html')
-        content = html_assembler.make_model(template)
-        html_filename = args.load_file.replace('.pkl', '.html')
-        with open(html_filename, 'w') as f:
-            f.write(content)
-        print(f"Generated HTML from {len(stmts)} Statements.")
-    elif args.load_file.endsiwth('.html'):
-        print("Using existing html file.")
-        html_filename = args.load_file
-    else:
-        print(f"Invalid input file: {args.input_file}")
-        sys.exit(1)
-    print(f"Please open {html_filename} in your browser.")
+    WORKING_DIR = path.abspath(args.working_dir)
 
     app.run()
-
-
-if __name__ == '__main__':
-    main()
