1a6c705d5f2c1441b7c7127c80785d0f4ecf8309
kkaris <karis.klas@gmail.com>
2025-02-05 16:01:21 -0800

    Add some tests for the validaton function

---
+++ tests/test_validation.py
@@ -0,0 +1,43 @@
+from curation_service.app import _validate_signor_comments
+
+
+def test_validate_signor_bad_syntax():
+    text = "abcd"
+    invalid_pattern, bad_keys = _validate_signor_comments(text)
+    assert invalid_pattern
+    assert isinstance(bad_keys, list)
+    assert len(bad_keys) == 0
+
+
+def test_validate_signor_bad_keys1a():
+    text = "key:value"
+    invalid_pattern, bad_keys = _validate_signor_comments(text)
+    assert isinstance(bad_keys, list)
+    assert len(bad_keys) == 1
+    assert bad_keys[0] == "key"
+
+
+def test_validate_signor_bad_keys1b():
+    text = "key:value;"
+    invalid_pattern, bad_keys = _validate_signor_comments(text)
+    assert not invalid_pattern
+    assert isinstance(bad_keys, list)
+    assert len(bad_keys) == 1
+    assert bad_keys[0] == "key"
+
+
+def test_validate_signor_bad_keys2():
+    text = "keya:value1;keyb:value2;keyc:value3"
+    invalid_pattern, bad_keys = _validate_signor_comments(text)
+    assert not invalid_pattern
+    assert isinstance(bad_keys, list)
+    assert len(bad_keys) == 3
+    assert bad_keys[0] == "keya"
+
+
+def test_validate_signor_valid():
+    text = "CELL:ABCD1234;EFFECT:increases;MECHANISM:phosphorylation"
+    invalid_pattern, bad_keys = _validate_signor_comments(text)
+    assert not invalid_pattern
+    assert isinstance(bad_keys, list)
+    assert len(bad_keys) == 0
