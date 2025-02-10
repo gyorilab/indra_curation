e191db2e7b5c5cf13372108a7cdcdffb2a27f0fc
kkaris <karis.klas@gmail.com>
2025-02-10 11:39:59 -0800

    Update tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -3,41 +3,43 @@ from curation_service.validation import validate_comment
 
 def test_validate_signor_bad_syntax():
     text = "abcd"
-    invalid_pattern, bad_keys = validate_comment(text)
-    assert invalid_pattern
-    assert isinstance(bad_keys, list)
-    assert len(bad_keys) == 0
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str)
+    assert len(error_msg) > 0
+    assert "Invalid syntax" in error_msg
 
 
 def test_validate_signor_bad_keys1a():
     text = "key:value"
-    invalid_pattern, bad_keys = validate_comment(text)
-    assert isinstance(bad_keys, list)
-    assert len(bad_keys) == 1
-    assert bad_keys[0] == "key"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str)
+    assert len(error_msg) > 0
+    assert "Invalid key(s)" in error_msg
 
 
 def test_validate_signor_bad_keys1b():
     text = "key:value;"
-    invalid_pattern, bad_keys = validate_comment(text)
-    assert not invalid_pattern
-    assert isinstance(bad_keys, list)
-    assert len(bad_keys) == 1
-    assert bad_keys[0] == "key"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str)
+    assert len(error_msg) > 0
+    assert "Invalid key(s)" in error_msg
 
 
 def test_validate_signor_bad_keys2():
     text = "keya:value1;keyb:value2;keyc:value3"
-    invalid_pattern, bad_keys = validate_comment(text)
-    assert not invalid_pattern
-    assert isinstance(bad_keys, list)
-    assert len(bad_keys) == 3
-    assert bad_keys[0] == "keya"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str)
+    assert len(error_msg) > 0
+    assert "Invalid key(s)" in error_msg
 
 
 def test_validate_signor_valid():
     text = "CELL:ABCD1234;EFFECT:increases;MECHANISM:phosphorylation"
-    invalid_pattern, bad_keys = validate_comment(text)
-    assert not invalid_pattern
-    assert isinstance(bad_keys, list)
-    assert len(bad_keys) == 0
+    valid_pattern, error_msg = validate_comment(text)
+    assert valid_pattern
+    assert isinstance(error_msg, str)
+    assert len(error_msg) == 0
