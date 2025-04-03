000590ff747105557f3ee7354ce22f886e4cc21d
kkaris <karis.klas@gmail.com>
2025-04-03 10:11:04 -0700

    Update tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -6,7 +6,7 @@ def test_validate_signor_bad_syntax():
     valid_pattern, error_msg = validate_comment(text)
     assert not valid_pattern
     assert isinstance(error_msg, str)
-    assert len(error_msg) > 0
+    assert len(error_msg) > 0, "Expected an error message to be returned"
     assert "Invalid syntax" in error_msg
 
 
@@ -14,35 +14,53 @@ def test_validate_signor_bad_keys1a():
     text = "key:value"
     valid_pattern, error_msg = validate_comment(text)
     assert not valid_pattern
-    assert isinstance(error_msg, str)
-    assert len(error_msg) > 0
-    assert "Invalid key(s)" in error_msg
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "No valid key-value pairs found." in error_msg, error_msg
 
 
 def test_validate_signor_bad_keys1b():
     text = "key:value;"
     valid_pattern, error_msg = validate_comment(text)
     assert not valid_pattern
-    assert isinstance(error_msg, str)
-    assert len(error_msg) > 0
-    assert "Invalid key(s)" in error_msg
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "No valid key-value pairs found." in error_msg, error_msg
 
 
 def test_validate_signor_bad_keys2():
     text = "keya:value1;keyb:value2;keyc:value3"
     valid_pattern, error_msg = validate_comment(text)
     assert not valid_pattern
-    assert isinstance(error_msg, str)
-    assert len(error_msg) > 0
-    assert "Invalid key(s)" in error_msg
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "No valid key-value pairs found." in error_msg, error_msg
+
+
+def test_validate_signor_bad_keys3():
+    text = "KEYA:value1;KEYB:value2;KEYC:value3"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "Invalid key(s):" in error_msg, error_msg
+
+
+def test_validate_signor_empty_value():
+    text = "CELL:;EFFECT:increases;MECHANISM:phosphorylation"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "Empty value for key(s)" in error_msg, error_msg
 
 
 def test_validate_signor_valid():
     text = "CELL:ABCD1234;EFFECT:increases;MECHANISM:phosphorylation"
     valid_pattern, error_msg = validate_comment(text)
     assert valid_pattern
-    assert isinstance(error_msg, str)
-    assert len(error_msg) == 0
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) == 0, error_msg
 
 
 def test_validate_signor_valid2():
@@ -51,5 +69,13 @@ def test_validate_signor_valid2():
     )
     valid_pattern, error_msg = validate_comment(text)
     assert valid_pattern
-    assert isinstance(error_msg, str)
-    assert len(error_msg) == 0
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) == 0, error_msg
+
+
+def test_validate_signor_valid_with_quotes():
+    text = 'CELL:"text;more text";EFFECT:increases;MECHANISM:phosphorylation'
+    valid_pattern, error_msg = validate_comment(text)
+    assert valid_pattern
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) == 0, error_msg
