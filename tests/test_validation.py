94b99b377a5d2208f58e798b68aeede90466cb19
kkaris <karis.klas@gmail.com>
2025-04-03 11:02:45 -0700

    Add more/update tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -19,13 +19,40 @@ def test_validate_signor_bad_syntax2():
     assert "No valid key-value pairs found." in error_msg, error_msg
 
 
+def test_validate_signor_bad_syntax3():
+    text = "CELL:fnejf;ccece:fffssffg fnmfnfefnkdfvmn n vnvrn;"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "Invalid key(s): ccece" in error_msg, error_msg
+
+
+def test_validate_signor_bad_syntax_spce_in_key():
+    text = "CELL   :fnejf;"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "One or more key(s) contain spaces: CELL" in error_msg, error_msg
+
+
+def test_validate_signor_bad_syntax_spce_in_key2():
+    text = "   CELL:fnejf;"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "One or more key(s) contain spaces: CELL" in error_msg, error_msg
+
+
 def test_validate_signor_bad_keys1a():
     text = "key:value"
     valid_pattern, error_msg = validate_comment(text)
     assert not valid_pattern
     assert isinstance(error_msg, str), error_msg.__class__
     assert len(error_msg) > 0, "Expected an error message to be returned"
-    assert "No valid key-value pairs found." in error_msg, error_msg
+    assert "Invalid key(s): key" in error_msg, error_msg
 
 
 def test_validate_signor_bad_keys1b():
@@ -34,7 +61,7 @@ def test_validate_signor_bad_keys1b():
     assert not valid_pattern
     assert isinstance(error_msg, str), error_msg.__class__
     assert len(error_msg) > 0, "Expected an error message to be returned"
-    assert "No valid key-value pairs found." in error_msg, error_msg
+    assert "Invalid key(s): key" in error_msg, error_msg
 
 
 def test_validate_signor_bad_keys2():
@@ -43,7 +70,7 @@ def test_validate_signor_bad_keys2():
     assert not valid_pattern
     assert isinstance(error_msg, str), error_msg.__class__
     assert len(error_msg) > 0, "Expected an error message to be returned"
-    assert "No valid key-value pairs found." in error_msg, error_msg
+    assert "Invalid key(s): keya, keyb, keyc" in error_msg, error_msg
 
 
 def test_validate_signor_bad_keys3():
@@ -52,7 +79,7 @@ def test_validate_signor_bad_keys3():
     assert not valid_pattern
     assert isinstance(error_msg, str), error_msg.__class__
     assert len(error_msg) > 0, "Expected an error message to be returned"
-    assert "Invalid key(s):" in error_msg, error_msg
+    assert "Invalid key(s): KEYA, KEYB, KEYC" in error_msg, error_msg
 
 
 def test_validate_signor_empty_value():
@@ -61,7 +88,34 @@ def test_validate_signor_empty_value():
     assert not valid_pattern
     assert isinstance(error_msg, str), error_msg.__class__
     assert len(error_msg) > 0, "Expected an error message to be returned"
-    assert "Empty value for key(s)" in error_msg, error_msg
+    assert "Empty value for key(s): CELL" in error_msg, error_msg
+
+
+def test_validate_signor_empty_value2():
+    text = "CELL:"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "Empty value for key(s): CELL" in error_msg, error_msg
+
+
+def test_validate_signor_empty_value3():
+    text = "CELL:;"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "Empty value for key(s): CELL" in error_msg, error_msg
+
+
+def test_validate_signor_empty_value4():
+    text = "CELL:;EFFECT:"
+    valid_pattern, error_msg = validate_comment(text)
+    assert not valid_pattern
+    assert isinstance(error_msg, str), error_msg.__class__
+    assert len(error_msg) > 0, "Expected an error message to be returned"
+    assert "Empty value for key(s): CELL, EFFECT" in error_msg, error_msg
 
 
 def test_validate_signor_valid():
